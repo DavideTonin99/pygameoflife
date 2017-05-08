c9270555a4958d5bb8f65f83de1d8f23a2c6ce76
DavideTonin99 <davide9935@gmail.com>
2017-05-08 19:50:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 """
 Simulation of Game of Life with pygame
+Instructions:
+Press ESC or F4 to quit the game
+Press RETURN to restart the game
+Press SPACE to stop or resume the game
+Press "p" or "+" to zoom in
+Press "m" or "-" to zoom out
+Press one of the letter below to change the color of the alive cells:
+    - r: red
+    - b: blue
+    - g: green
+    - c: cyan
+    - w: white
+When the game is stopped, you can click and move the mouse to select new cells
 """
 import numpy as np
 import pygame
